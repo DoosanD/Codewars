ad7a49662dd5e57dafb6e39aabf1926080f5aef9
DoosanD <davicdusan@gmail.com>
2022-05-22 14:14:47 +0200

    num/string

---
+++ numberToString/script.js
@@ -0,0 +1,18 @@
+function numberToString(num) {
+  // Return a string of the number here!
+  return num.toString();
+}
+
+// 1001 should return "1001"
+console.log((x = numberToString(1001)));
+console.log(typeof x);
+
+// Return a number of the string now!
+const stringToNumber = function (str) {
+  // put your code here
+  return Number(str);
+};
+
+// 1001 should return as a number now
+console.log((x = stringToNumber(x)));
+console.log(typeof x);
