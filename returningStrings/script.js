e7f2812a37e19c5b59b282d0bcfc1a67db0b90e5
DoosanD <davicdusan@gmail.com>
2022-05-26 21:35:32 +0200

    чет, 26. мај 2022.  21:35:32 CEST

---
+++ returningStrings/script.js
@@ -0,0 +1,10 @@
+// Make a function that will return a greeting statement that uses an input;
+// your program should return, "Hello, <name> how are you doing today?".
+
+function greet(name) {
+  //template literal ${} with backticks ``
+  return `Hello, ${name} how are you doing today?`;
+}
+
+// My name :)
+console.log(greet("Dusan Davic"));
