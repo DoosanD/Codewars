4ec5069dd23dd734141e4aee9697b9ccba3239b8
DoosanD <davicdusan@gmail.com>
2022-05-27 16:08:49 +0200

    05/27/22-05-22

---
+++ cantSleepCountSheep/script.js
@@ -0,0 +1,19 @@
+// If you can't sleep, just count sheep!!
+
+// Given a non-negative integer, 3 for example,
+// return a string with a murmur: "1 sheep...2 sheep...3 sheep...".
+// Input will always be valid, i.e. no negative integers.
+
+const countSheep = function (num) {
+  let string = "";
+  // If zero was input return empty string
+  if (num == 0) {
+    return "";
+  } else {
+    // For every unit return index with string "sheep...""
+    for (let i = 0; i < num; i++) {
+      string += `${i + 1} sheep...`;
+    }
+    return string;
+  }
+};
