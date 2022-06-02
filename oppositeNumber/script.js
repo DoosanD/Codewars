8f3b4afeb5b67875d16b5e4826ac86d8998b82c8
DoosanD <davicdusan@gmail.com>
2022-06-02 17:36:49 +0200

    2022-02-06

---
+++ oppositeNumber/script.js
@@ -0,0 +1,14 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+function opposite(number) {
+  return number * -1;
+}
+
+// Returns
+console.log(opposite(1));
+
+// Returns
+console.log(opposite(14));
+
+// Returns
+console.log(opposite(-34));
