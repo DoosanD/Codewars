9791f07992c17ca58da80570f57d6b90c149f6a4
DoosanD <davicdusan@gmail.com>
2022-06-28 14:59:16 +0200

    2022-28-06

---
+++ camelCase/script.js
@@ -0,0 +1,22 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+// Example
+
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+function solution(string) {
+  string = string.split("").map(function (el) {
+    if (el === el.toUpperCase()) {
+      el = " " + el;
+    }
+    return el;
+  });
+  return string.join("");
+}
+
+console.log(solution("camelCase"));
+
+console.log(solution("identifier"));
+
+console.log(solution(""));
